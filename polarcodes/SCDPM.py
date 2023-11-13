c14e4ae7414b635415eca9c4b7bf74804b6ef9a4
Väisänen, Petri <petri.vaisanen@nordicsemi.no>
2023-11-13 18:25:45 +0200

    Path-metric computation added to SCDPM

---
+++ polarcodes/SCDPM.py
@@ -0,0 +1,288 @@
+import numpy as np
+from polarcodes.Tables import Q_Nmax
+
+
+class SCDPM:
+    def __init__(self, N, K):
+        # Params
+        self.N = N
+        self.n = int(np.log2(N))
+        self.K = K
+        # Get the Polar sequence for N
+        self.Q_N = Q_Nmax[Q_Nmax < N]
+        print("Q_N: {}".format(self.Q_N))
+        # DUMMY: Get the information bit pattern.
+        self.Q_I = np.ones(N, dtype=int)
+        self.F = N - K
+        self.Q_I[self.Q_N < self.F] = 0
+        print("Q_I: {}".format(self.Q_I))
+        # LLRs and partial sums
+        self.L = np.zeros((self.N, self.n+1), dtype=float)
+        self.s_hat = np.zeros((self.N, self.n), dtype=int)
+        self.PM = float(0)
+
+    def encode(self, a):
+        """
+        Encoding for Polar codes
+
+        Parameters
+        ----------
+        a: ndarray<int>, int
+            a vector of info bits
+
+        Returns
+        ----------
+        x: ndarray<int>, int
+            a vector of encoded bits
+
+        """
+        # Frozen bits insertion
+        u = np.zeros(self.N, dtype=int)
+        u[self.Q_I == 1] = a
+        print("u:\n{}".format(u))
+        # Polar encoding
+        s = np.zeros((self.N, self.n+1), dtype=int)
+        s[:, 0] = u
+        for l in np.arange(self.n, dtype=int):
+            s_block = 1 << l
+            i_step = 1 << (l+1)
+            for i_block in np.arange(0, self.N, i_step, dtype=int):
+                for i in np.arange(s_block):
+                    s[i_block + i, l+1] = \
+                        s[i_block + i, l] ^ s[i_block + i + s_block, l]
+                    s[i_block + i + s_block, l+1] = s[i_block + i + s_block, l]
+        # print("s:\n{}".format(s))
+        x = s[:, self.n]
+        return x
+
+    def decode(self, sbit):
+        """
+        Successive cancellation decoding for Polar codes
+
+        Parameters
+        ----------
+        sbit: ndarray<float>, float
+            a vector of channel LLRs
+
+        Returns
+        ----------
+        a_hat: ndarray<int>, int
+            a vector of decoded info bits
+
+        """
+        # Polar decoding
+        self.L[:, self.n] = sbit
+        for i in np.arange(self.N, dtype=int):
+            # LLR and bit levels
+            a_llr = active_llr_level(bit_reversed(i, self.n), self.n)
+            a_bit = active_bit_level(bit_reversed(i, self.n), self.n)
+            # LLR computation
+            self.update_llrs(i, a_llr)
+            # Decision
+            if self.Q_I[i] == 0:
+                # Frozen bit
+                self.s_hat[i, 0] = 0
+            else:
+                # Hard decision
+                self.s_hat[i, 0] = hard_decision(self.L[i, 0])
+            # Partial sum computation
+            self.update_bits(i, a_bit)
+            print("PM: {:.2f}".format(self.PM))
+        a_hat = self.s_hat[self.Q_I == 1, 0]
+        return a_hat
+
+    def update_llrs(self, i, a_llr):
+        """
+        LLR computations
+
+        Parameters
+        ----------
+        i: int
+            bit index
+        a_llr: int
+            LLR level index
+
+        Returns
+        ----------
+        """
+        for l in np.arange(a_llr, -1, -1):
+            s_block = 1 << l
+            for i_b in np.arange(s_block):
+                # Select upper or lower computation
+                B = (i >> l) & 1
+                if B == 0:
+                    # Upper (f)
+                    self.L[i + i_b, l] = \
+                        upper_llr_approx(
+                            self.L[i + i_b, l+1],
+                            self.L[i + i_b + s_block, l+1])
+                else:
+                    # Lower (g)
+                    self.L[i + i_b, l] = \
+                        lower_llr(
+                            self.s_hat[i + i_b - s_block, l],
+                            self.L[i + i_b - s_block, l+1],
+                            self.L[i + i_b, l+1])
+
+    def update_bits(self, i, a_bit):
+        """
+        Partial sum computations
+
+        Parameters
+        ----------
+        i: int
+            bit index
+        a_bit: int
+            bit level index
+
+        Returns
+        ----------
+        """
+        self.update_pm_hwf(i, 0)
+        for l in np.arange(a_bit):
+            s_block = 1 << l
+            for i_b in np.arange(s_block):
+                self.s_hat[i - i_b - s_block, l+1] = \
+                    self.s_hat[i - i_b - s_block, l] ^ self.s_hat[i - i_b, l]
+                self.update_pm_hwf(i - i_b - s_block, l+1)
+                self.s_hat[i - i_b, l+1] = self.s_hat[i - i_b, l]
+                self.update_pm_hwf(i - i_b, l+1)
+
+    def update_pm_hwf(self, i, l):
+        """
+        Path-metric update (HW friendly)
+
+        Parameters
+        ----------
+        i: int
+            bit index
+        l: int
+            stage index
+
+        Returns
+        ----------
+        """
+        self.PM += (
+            np.sign(self.L[i, l]) * self.L[i, l] -
+            ((1 - 2 * self.s_hat[i, l]) * self.L[i, l]))
+
+
+def bit_reversed(x, n):
+    """
+    Bit-reversal operation.
+
+    Parameters
+    ----------
+    x: ndarray<int>, int
+        a vector of indices
+    n: int
+        number of bits per index in ``x``
+
+    Returns
+    ----------
+    ndarray<int>, int
+        bit-reversed version of x
+
+    """
+    result = 0
+    for i in range(n):  # for each bit number
+        if (x & (1 << i)):  # if it matches that bit
+            result |= (1 << (n - 1 - i))  # set the "opposite" bit in result
+    return result
+
+
+def active_llr_level(i, n):
+    """
+        Find the first 1 in the binary expansion of i.
+    """
+    mask = 2**(n-1)
+    for k in range(n):
+        if (mask & i) == 0:
+            mask >>= 1
+        else:
+            break
+    return k
+
+
+def active_bit_level(i, n):
+    """
+        Find the first 0 in the binary expansion of i.
+    """
+    mask = 2**(n-1)
+    for k in range(n):
+        if (mask ^ (i & mask)) == 0:
+            mask >>= 1
+        else:
+            break
+    return k
+
+
+def upper_llr_approx(l1, l2):
+    """
+    Update top branch LLR in the log-domain (approximation).
+    This function supports shortening by checking for infinite LLR cases.
+
+    Parameters
+    ----------
+    l1: float
+        input LLR corresponding to the top branch
+    l2: float
+        input LLR corresponding to the bottom branch
+
+    Returns
+    ----------
+    float
+        the top branch LLR at the next stage of the decoding tree
+
+    """
+    # check for infinite LLR, used in shortening
+    if l1 == np.inf and l2 != np.inf:
+        return l2
+    elif l1 != np.inf and l2 == np.inf:
+        return l1
+    elif l1 == np.inf and l2 == np.inf:
+        return np.inf
+    else:  # principal decoding equation
+        return np.sign(l1) * np.sign(l2) * np.min((np.abs(l1), np.abs(l2)))
+
+
+def lower_llr(b, l1, l2):
+    """
+    Update bottom branch LLR in the log-domain.
+    This function supports shortening by checking for infinite LLR cases.
+
+    Parameters
+    ----------
+    b: int
+        the decoded bit of the top branch
+    l1: float
+        input LLR corresponding to the top branch
+    l2: float
+        input LLR corresponding to the bottom branch
+
+    Returns
+    ----------
+    float, np.nan
+        the bottom branch LLR at the next stage of the decoding tree
+    """
+    if b == 0:  # check for infinite LLRs, used in shortening
+        if l1 == np.inf or l2 == np.inf:
+            return np.inf
+        else:
+            # g(b, l1, l2) = (-1)^b * l1 + l2
+            return l1 + l2
+    elif b == 1:
+        # g(b, l1, l2) = (-1)^b * l1 + l2
+        return -l1 + l2
+    else:
+        return np.nan
+
+
+def hard_decision(y):
+    """
+        Hard decision of a log-likelihood.
+    """
+    if y >= 0:
+        return 0
+    else:
+        return 1
---
+++ test_polar_scld.py
@@ -0,0 +1,63 @@
+import numpy as np
+import argparse
+
+import polarcodes.SCDPM as PC
+
+np.set_printoptions(precision=2, suppress=True)
+# np.set_printoptions(precision=2)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-N", "--l_enc", type=int)
+parser.add_argument("-K", "--l_info", type=int)
+parser.add_argument("-E", "--esn0", type=float)
+parser.add_argument("-S", "--seed", type=int)
+
+
+def main(N, K, EsN0dB, seed=0):
+
+    # Arguments:
+    n = int(np.log2(N))
+    print("Polar: N: {}, n: {}, K: {}, EsN0: {} dB, seed: {}".format(
+        N, n, K, EsN0dB, seed))
+
+    # Random seed
+    np.random.seed(seed)
+
+    # SCD for Polar codes PC(N, K)
+    m_scld = PC.SCDPM(N, K)
+
+    # Info bits
+    a = np.random.randint(2, size=K)
+    print("a:\n{}".format(a))
+
+    # Encode
+    x = m_scld.encode(a)
+    print("x:\n{}".format(x))
+
+    # AWGN
+    scale = 10**(-EsN0dB / 20)
+    awgn = scale * np.random.randn(N)
+    y = (1.0 - 2.0 * x) + awgn
+    print("y:\n{}, awgn: {:.2f}".format(y, scale))
+
+    # Channel LLRs aka BPSK softbit demodulation and demapper
+    norm = 10**(-EsN0dB / 10)
+    sbit = (2 / norm) * y
+    print("sbit:\n{}, norm: {:.2f}".format(sbit, norm))
+
+    # Decode
+    a_hat = m_scld.decode(sbit)
+
+    # Results
+    print("L:\n{}".format(m_scld.L))
+    print("s_hat:\n{}".format(m_scld.s_hat))
+    print("PM: {:.2f}".format(m_scld.PM))
+    print("SYMS: {}".format(np.all(np.sign(sbit) == (1.0 - 2.0 * x))))
+    print("a:\n{}".format(a))
+    print("a_hat:\n{}".format(a_hat))
+    print("PASS: {}".format(np.all(a_hat == a)))
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args.l_enc, args.l_info, args.esn0, args.seed)
